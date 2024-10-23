36f636dcc756195ae75155a5663befbe7b23b0f5
Acer <Acer@DESKTOP-ABKHEEV>
2024-10-23 15:52:03 +0530

    Add project files.

---
+++ ClassObj231024/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObj231024
+{
+    internal class Circle
+    {
+        float r;
+        public void SetRadius(float radius)//method1
+        {
+            r = radius;
+        }
+        public void ShowArea()//method2
+        {
+            float result = 3.14f * r * r;
+            Console.WriteLine("Area of Circle :"+result);
+        }
+    }
+}
---
+++ ClassObj231024/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassObj231024
+{
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1=new Circle();
+            Console.WriteLine("Enter radius of Circle");
+            float r1=float.Parse(Console.ReadLine());   
+            c1.SetRadius(r1);
+            c1.ShowArea();
+        }
+    }
+}
